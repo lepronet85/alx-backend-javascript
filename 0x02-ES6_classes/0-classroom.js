421fc4687ad8cbe8eba574e65c77f56fdeff9d94
Abdoul <lepronet85@gmail.com>
2024-06-27 17:00:55 +0100

    Add task 1 file

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,6 @@
+export default class ClassRoom {
+  // Constructor method that initializes the _maxStudentsSize property
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
